8c5f00ff15318a3f76ac10eeea6f5061560ba21f
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-22 12:55:22 +0300

    form validatioin and errors at specific fields

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,9 +1,11 @@
 <h1>Login page</h1>
-{{$errors}}
+
 <form action="/" method="POST">
     @csrf
     <input type="text" name="username" placeholder="Enter your name"/><br/><br/>
+    <span style="color:red">@error('username') {{$message}} @enderror </span><br/><br/>
     <input type="password" name="password" placeholder="Enter your password"/><br/><br/>
+    <span style="color:red">@error('password') {{$message}}@enderror </span><br/><br/>
     <button type="submit">Login</button>
 
 </form>

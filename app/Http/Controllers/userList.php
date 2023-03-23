6d04741ea10c10051a78dac4393302e983154507
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-23 12:08:12 +0300

    retrieve data from db

--- app/Http/Controllers/userList.php
+++ app/Http/Controllers/userList.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class userList extends Controller
 {
     function list(){
-        return view('list');
+        $data= Member::all();
+        return view('list',['members'=>$data]);
     }
 }
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,1 +1,16 @@
 <h1>Welcome List</h1>
+<table border="1">
+    <tr>
+        <td>id</td>
+        <td>name</td>
+        <td>Location</td>
+    </tr>
+    @foreach($members as $member)
+    <tr>
+        <td>{{$member['id']}} </td>
+        <td>{{$member['name']}}</td>
+        <td>{{$member['location']}}</td>
+    </tr>
+    @endforeach
+
+</table>
